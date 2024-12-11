e8d673e29aa31227df77620b5f67080346e51bcb
blksy <79380937+blksy@users.noreply.github.com>
2024-12-11 22:51:34 +0100

    modified About page and HomeLayout, added a Footer

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,20 +2,24 @@ import Logo from "../assets/Logo.png";
 
 const About = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-bl from-blue-300 via-blue-500 to-blue-400">
-      <img src={Logo} alt="Logo" className="w-72 h-72 mb-8" />
-      <p className="text-center px-6 max-w-2xl text-xl text-white">
-        Our app is your ultimate portal for home services, connecting skilled
-        specialists with clients in need of quality solutions. Whether you're
-        looking to book a professional for cleaning, renovation, repairs, or any
-        other home-related service, our platform simplifies the process. We
-        empower clients to find trusted experts and specialists to showcase
-        their skills and grow their business, all in one convenient space. With
-        user-friendly tools for posting jobs, managing inquiries, and
-        facilitating seamless communication, our app ensures a hassle-free
-        experience for everyone involved. Transforming homes has never been
-        easier!
-      </p>
+    <div className="flex flex-row items-center justify-center h-screen bg-gradient-to-bl from-blue-300 via-blue-500 to-blue-400 px-6">
+      <div className="flex-1 flex justify-center">
+        <img src={Logo} alt="Logo" className="w-90 h-90 " />
+      </div>
+      <div className="flex-1 text-right max-w-2xl">
+        <p className="text-xl text-white pr-72">
+          Our app is your ultimate portal for home services, connecting skilled
+          specialists with clients in need of quality solutions. Whether you're
+          looking to book a professional for cleaning, renovation, repairs, or
+          any other home-related service, our platform simplifies the process.
+          We empower clients to find trusted experts and specialists to showcase
+          their skills and grow their business, all in one convenient space.
+          With user-friendly tools for posting jobs, managing inquiries, and
+          facilitating seamless communication, our app ensures a hassle-free
+          experience for everyone involved. Transforming homes has never been
+          easier!
+        </p>
+      </div>
     </div>
   );
 };
--- src/pages/HomeLayout.tsx
+++ src/pages/HomeLayout.tsx
@@ -2,21 +2,28 @@ import { Outlet } from "react-router-dom";
 import SideNav from "../components/SideNav";
 import Navbar from "../components/Navbar";
 import { useState } from "react";
+import Footer from "../components/Footer";
 
 const HomeLayout = () => {
   const [sideNavToggle, setSideNavToggle] = useState(false);
 
   return (
-    <div className="flex">
-      {sideNavToggle && <SideNav sideNavToggle={sideNavToggle} />}
-      <div className="flex-1">
-        <Navbar
-          sideNavToggle={sideNavToggle}
-          setSideNavToggle={setSideNavToggle}
-        />
-        <Outlet />
+    <div className="flex flex-col min-h-screen">
+      <div className="flex flex-1">
+        {sideNavToggle && <SideNav sideNavToggle={sideNavToggle} />}
+        <div className="flex-1">
+          <Navbar
+            sideNavToggle={sideNavToggle}
+            setSideNavToggle={setSideNavToggle}
+          />
+          <div>
+            <Outlet />
+          </div>
+        </div>
       </div>
+      <Footer />
     </div>
   );
 };
+
 export default HomeLayout;
