afcf038983f1af564b4909665f0d0911059dfd3c
blksy <79380937+blksy@users.noreply.github.com>
2024-12-19 12:37:27 +0100

    implemented login via google provider

--- src/pages/Start.tsx
+++ src/pages/Start.tsx
@@ -1,8 +1,22 @@
 import { Link } from "react-router-dom";
 import Bg from "../assets/Start.bg.jpg";
 import { FcGoogle } from "react-icons/fc";
+import { supabase } from "../database/supabase";
 
 const Start = () => {
+  const handleGoogleLogin = async () => {
+    const { error } = await supabase.auth.signInWithOAuth({
+      provider: "google",
+      options: {
+        redirectTo: `${window.location.origin}/app`,
+      },
+    });
+
+    if (error) {
+      console.error("Error logging in with Google:", error.message);
+    }
+  };
+
   return (
     <div
       className="page-container-col"
@@ -34,7 +48,10 @@ const Start = () => {
           >
             Register
           </Link>
-          <button className="flex items-center justify-center bg-white text-gray-800 px-6 py-3 rounded-lg text-lg border border-gray-300 shadow transition duration-300 ease-in-out hover:bg-gray-100">
+          <button
+            onClick={handleGoogleLogin}
+            className="flex items-center justify-center bg-white text-gray-800 px-6 py-3 rounded-lg text-lg border border-gray-300 shadow transition duration-300 ease-in-out hover:bg-gray-100"
+          >
             <FcGoogle className="mr-2 text-2xl" /> {/* Google icon */}
             Login with Google
           </button>
