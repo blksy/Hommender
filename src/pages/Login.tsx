b28701548f1881ae7e6238318d7d833ede0b2409
blksy <79380937+blksy@users.noreply.github.com>
2025-01-11 17:21:37 +0100

    added login function from useAuth in Login.tsx, added toast and replaced alerts with it , removed link from Login button to prevent navigation to the home page without login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -4,9 +4,13 @@ import { loginSchema } from "../validators";
 import { FormInput } from "../components/FormInput";
 import { LoginFormValues } from "../../types/types";
 import FormLayout from "../components/FormLayout";
+import { useAuth } from "../hooks/useAuth";
+import { ROUTES } from "../router/routes";
+import toast, { Toaster } from "react-hot-toast";
 
 const Login = () => {
   const navigate = useNavigate();
+  const { login } = useAuth();
 
   const formik = useFormik<LoginFormValues>({
     initialValues: {
@@ -14,19 +18,29 @@ const Login = () => {
       password: "",
     },
     validationSchema: loginSchema,
-    onSubmit: async (values) => {},
+    onSubmit: async (values) => {
+      try {
+        await login(values.email, values.password);
+        toast.success("Login successful! Redirecting to the main page...");
+        navigate(ROUTES.APP);
+      } catch (err) {
+        toast.error("Error logging in: " + (err as Error).message);
+      }
+    },
   });
 
   return (
-    <FormLayout title="Provide user credentials to login">
+    <FormLayout
+      title="Provide user credentials to login"
+      onSubmit={formik.handleSubmit}
+    >
+      <Toaster position="top-center" reverseOrder={false} />
       <FormInput formik={formik} accessor="email" label="Email" />
       <FormInput formik={formik} accessor="password" label="Password" />
       <div className="flex flex-col gap-4 mt-4 items-center">
-        <Link to={"/app"}>
-          <button type="submit" className="btn-primary">
-            Login
-          </button>
-        </Link>
+        <button type="submit" className="btn-primary">
+          Login
+        </button>
         <Link to={"/"}>
           <button type="button" className="btn-secondary">
             Back
