39141db973a8107772cb68eac3ee0f749863118b
blksy <79380937+blksy@users.noreply.github.com>
2025-01-22 20:38:58 +0100

    added functions in usersRequests.tsx

--- src/api/usersRequests.tsx
+++ src/api/usersRequests.tsx
@@ -1,1 +1,54 @@
 import { User } from "../../types/types";
+import { supabase } from "../database/supabase";
+
+export const fetchUsers = async () => {
+  try {
+    const { data: users, error } = await supabase.from("users").select("*");
+    if (error) {
+      console.error("Failed to fetch users", error);
+      throw error;
+    }
+    return users;
+  } catch (error) {
+    console.error("Unexpected error fetching users", error);
+    throw error;
+  }
+};
+
+export const getUserById = async (id: string) => {
+  try {
+    const { data, error } = await supabase
+      .from("users")
+      .select("id, name, username, email")
+      .eq("id", id)
+      .single();
+
+    if (error) {
+      console.error("Failed to fetch user by ID", error);
+      throw error;
+    }
+
+    return data;
+  } catch (error) {
+    console.error("Unexpected error fetching user by ID", error);
+    throw error;
+  }
+};
+
+export const updateUserById = async (
+  updateUserData: Partial<User>,
+  id: string | number
+) => {
+  const { data, error } = await supabase
+    .from("users")
+    .update(updateUserData)
+    .eq("id", id)
+    .select();
+
+  if (error) {
+    console.error("Failed to update client data", error);
+    throw error;
+  }
+
+  return data;
+};
