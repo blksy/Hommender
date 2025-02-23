33724ef2cb6ab682f6bf8b4fb07d2c6325245090
blksy <79380937+blksy@users.noreply.github.com>
2025-02-23 19:37:35 +0100

    deleted link items

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,12 +3,6 @@ import Logo from "../assets/Logo.png";
 import Bg from "../assets/Home.bg.jpg";
 import { useUser } from "../context/UserContext";
 
-const linkItems = [
-  { to: "/app/services/add_service", label: "Post a Service" },
-  { to: "/app/requests/add_request", label: "Post a Request" },
-  { to: "/app/specialists", label: "Contact Specialists" },
-];
-
 const Home = () => {
   const { user } = useUser();
 
