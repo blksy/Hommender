019769a9383ea59cf9da6ca93e912bb0ce8c34a4
blksy <79380937+blksy@users.noreply.github.com>
2025-05-22 20:42:42 +0200

    fixing errors

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import emailjs from "emailjs-com";
+import emailjs from "@emailjs/browser";
 import Bg from "../assets/Contact.bg.jpg";
 import { toast, Toaster } from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
