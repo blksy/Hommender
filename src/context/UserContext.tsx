13bd445f1c3d585cd311eefaecaa25bb0fda0d34
blksy <79380937+blksy@users.noreply.github.com>
2025-07-05 15:52:11 +0200

    .

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -16,13 +16,11 @@ export const UserProvider: React.FC<{ children: ReactNode }> = ({
 }) => {
   const [user, setUser] = useState<User | Client | Specialist | null>(null);
 
-  //const { data: { user } } = await supabase.auth.getUser()
-
   useEffect(() => {
     supabase.auth.getSession().then(() => {
       fetchUserData()
         .then((data) => {
-          console.log("userData", data);
+          // console.log("userData", data);
           setUser(data);
         })
         .catch(() => {
@@ -33,7 +31,7 @@ export const UserProvider: React.FC<{ children: ReactNode }> = ({
     supabase.auth.onAuthStateChange(async () => {
       fetchUserData()
         .then((data) => {
-          console.log("userData", data);
+          // console.log("userData", data);
           setUser(data);
         })
         .catch(() => {
@@ -41,32 +39,6 @@ export const UserProvider: React.FC<{ children: ReactNode }> = ({
         });
     });
   }, []);
-
-  // supabase.auth.onAuthStateChange(async (event) => {
-  //   try {
-  //     console.log("onAuthStateChange");
-  //     const { data: sessionData, error } = await supabase.auth.getSession();
-  //     console.log("sessionData", sessionData);
-  //     if (error) throw error;
-
-  //     const session = sessionData?.session;
-
-  //     if (
-  //       session?.user &&
-  //       (event === "SIGNED_IN" ||
-  //         event === "USER_UPDATED" ||
-  //         event === "TOKEN_REFRESHED")
-  //     ) {
-  //       const userData = await fetchUserData();
-  //       setUser(userData);
-  //     } else if (event === "SIGNED_OUT") {
-  //       setUser(null);
-  //     }
-  //   } catch (error) {
-  //     console.error("Error fetching session in auth state change:", error);
-  //     setUser(null);
-  //   }
-  // });
 
   return (
     <UserContext.Provider value={{ user, fetchUserData }}>
